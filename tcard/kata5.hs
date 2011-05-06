0254ef8e987f7686fa0abadeb6127b969ee32ea8
tonicardenas@gmail.com <tonicardenas@gmail.com>
2011-05-06 21:03:29 +0800

    Kata de mayo en Haskell.

--- tcard/kata5.hs
+++ tcard/kata5.hs
@@ -1,5 +1,5 @@
 data Pescado = Vieira | Pulpo | Centollo
-data Ciudad = Madrid | Barcelona | Lisboa
+data Ciudad = Madrid | Barcelona | Lisboa deriving (Show)
 
 precio :: Pescado -> Ciudad -> Integer
 precio pescado ciudad = case pescado of
@@ -34,8 +34,10 @@ coste ciudad = 5 + 2 * distancia ciudad
 beneficios :: [(Pescado, Integer)] -> Ciudad -> Float
 beneficios carga ciudad = ingresos carga ciudad - fromIntegral (coste ciudad)
 
+mayorBeneficio :: [(Pescado, Integer)] -> [Ciudad] -> Ciudad
+mayorBeneficio carga ciudades =
+	foldl (\x y -> if beneficios carga x > beneficios carga y then x else y) (head ciudades) (tail ciudades)
+	
 main = do
-	putStrLn . show $ beneficios carga Madrid
-	putStrLn . show $ beneficios carga Barcelona
-	putStrLn . show $ beneficios carga Lisboa
-		where carga = [(Vieira, 50), (Pulpo, 100), (Centollo, 50)]
+	putStrLn . show $ mayorBeneficio carga [Madrid, Lisboa, Barcelona]
+		where 	carga = [(Vieira, 50), (Pulpo, 100), (Centollo, 50)]
